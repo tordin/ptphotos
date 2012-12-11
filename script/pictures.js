fe1c32e7000b6bc36aa1897ec0f77ab3d648cf0a
Luiz Tordin <tordin@gmail.com>
2012-12-10 23:09:50 -0200

    esq dir

--- script/pictures.js
+++ script/pictures.js
@@ -1,7 +1,40 @@
 function Album(settings) {
 	var $t = this;
 	
-	function renderPicture(picture) {
+	var last_loaded_picture = 0,
+		pictures_per_page = 5,
+		pictures = [],
+		current_picture = -1;
+		
+	function navigate(delta) {
+		if (current_picture + delta >= 0 && current_picture + delta < last_loaded_picture) {
+			open(current_picture + delta);			
+		}
+	}
+	
+	function open(i) {
+		current_picture = i;
+
+		$('#large_picture_src').attr('src', pictures[i].picture_url);
+		
+		$('.img-control').unbind('click');
+		
+		$('.img-control.previous').click(function() {
+			navigate(-1);
+		});
+		
+		$('.img-control.next').click(function() {
+			navigate(1);
+		});
+		
+	    $('#overlay').fadeIn('fast');
+	 
+	    $('#picture_preview').show().animate({
+	        'top': '-15px'
+	    }, 500);
+	}
+
+	function renderPicture(picture, i) {
 		var html = $('<div/>');
 		
 		html.addClass('picture')
@@ -9,33 +42,26 @@ function Album(settings) {
 			$('<img/>')
 				.attr('src', picture.thumb_url)
 		).click(function() {
-			$('#large_picture_src').attr('src', picture.picture_url);
-			
-		    $('#overlay').fadeIn('fast');
-		 
-		    $('#picture_preview').show().animate({
-		        'top':'-15px'
-		    }, 500);
+			open(i);
 		});
 		
 		return html;
 	}
 	
-	var last_picture = 0,
-		pictures_per_page = 5; 
-
 	function loadPictures() {
-		settings.load(last_picture, pictures_per_page, function(pictures) {
-			pictures.forEach(function(picture) {
-				$('#' + settings.target + ' .load_more').before(renderPicture(picture));
+		settings.load(last_loaded_picture, pictures_per_page, function(loaded_pictures) {
+			var i = pictures.length;
+			pictures = pictures.concat(loaded_pictures);
+			
+			loaded_pictures.forEach(function(picture, k) {
+				$('#' + settings.target + ' .load_more').before(renderPicture(picture, i + k));
 			});
 		
-			last_picture += pictures_per_page;
+			last_loaded_picture += pictures_per_page;
 		});
 	}
 
-	$('#' + settings.target).empty()
-	.append(
+	$('#' + settings.target).empty().append(
 		$('<div/>')
 			.addClass('load_more')
 			.append(
